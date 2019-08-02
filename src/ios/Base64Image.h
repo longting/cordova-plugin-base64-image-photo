2361c9bca7eac3960dfa88cc9a58f366f4244d4e
longting <longting@huishidai.com>
2019-08-02 19:15:51 +0800

    ios

---
+++ src/ios/Base64Image.h
@@ -0,0 +1,16 @@
+//
+//  Base64Image.h
+//  cordovaTest
+//
+//  Created by huishidai on 2019/8/2.
+//
+
+#import "Cordova/CDVPlugin.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Base64Image : CDVPlugin
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/ios/Base64Image.m
@@ -0,0 +1,33 @@
+//
+//  Base64Image.m
+//  cordovaTest
+//
+//  Created by huishidai on 2019/8/2.
+//
+
+#import "Base64Image.h"
+
+@implementation Base64Image
+
+-(void) save:(CDVInvokedUrlCommand *)command{
+    if(command.arguments.count>0){
+        NSString* base64 = command.arguments[0];
+        
+        NSArray *imageArray = [base64 componentsSeparatedByString:@","];
+        
+        NSData *imageData = [[NSData alloc] initWithBase64EncodedString:imageArray[1] options:NSDataBase64DecodingIgnoreUnknownCharacters];
+        
+        UIImage *image = [UIImage imageWithData:imageData];
+        
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+       
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"保存图片"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }else{
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"没有参数"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }
+    
+}
+
+@end
