64931c8149a0d3fc68ab439be96899c3576f809e
longting <longtinghappy@163.com>
2019-08-27 11:10:28 +0800

    优化

--- www/cordova-plugin-base64-image-photo.js
+++ www/cordova-plugin-base64-image-photo.js
@@ -1,24 +1,10 @@
 //一个exports.XXX代表插件的一个方法，exports后面和exec方法的参数4都是方法名称
 var exec = require('cordova/exec');
 
-var pluginName = 'Base64Image';
-
-let Base64Image = {
-	save(base64Str, success, error) {
-		//参数1和参数2分别是调用插件成功和失败的回调方法（js）
-	    //参数3是插件名
-	    //参数4是方法名
-	    //参数5是js的传参
-        exec(success, error, pluginName , 'save', [base64Str]);
-    },
+exports.save = function (base64Str, success, error) {
+    //参数1和参数2分别是调用插件成功和失败的回调方法（js）
+    //参数3是插件名
+    //参数4是方法名
+    //参数5是js的传参
+	exec(success, error, 'Base64Image' , 'save', [base64Str]);
 };
-module.exports = Base64Image;
-
-
-// exports.Base64Image = function (arg0, success, error) {
-//     //参数1和参数2分别是调用插件成功和失败的回调方法（js）
-//     //参数3是插件名
-//     //参数4是方法名
-//     //参数5是js的传参
-//     exec(success, error, pluginName , 'save', [arg0]);
-// };
